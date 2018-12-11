2fc5557df4b91a3de35ae9b2e666064df4656470
jupterorbita <john.williams@outlook.com>
2018-12-10 21:50:43 -0700

    added Classes, SubClasses, Modules

--- main.js
+++ main.js
@@ -17,7 +17,6 @@ nums.push(5);
 // console.log(nums);
 
 // ======== ARROW FUNCTIONS ========
-
 // function sayHello() {
 //   console.log("Hello");
 // }
@@ -56,7 +55,6 @@ const people2 = people.filter(person => person.id !== 2);
 // console.log(people2);
 
 // ======== SPREAD ========
-
 const arr = [1, 2, 3];
 // const arr1 = [arr, 4];
 const arr2 = [...arr, 4];
@@ -74,6 +72,7 @@ const person2 = {
   email: "john@somewhere.com"
 };
 // console.log(person2);
+
 // ======== DESTRUCTURING ========
 const profile = {
   name: "John Doe",
@@ -91,10 +90,66 @@ const { name, address, hobbies } = profile; //pulling name out of profile
 
 // if we want just the street
 const { street, city } = profile.address;
-console.log(street, city);
+// console.log(street, city);
 
 // ======== CLASSES ========
 
+// class Person {
+//   constructor() {
+//     console.log("ran");
+//     this.name = "JOHN";
+//   }
+// }
+
+class Person {
+  constructor(name_to_pass_in, age) {
+    this.name = name_to_pass_in;
+    this.age = age;
+  }
+
+  //methods
+  greet() {
+    return `hello, my name is ${this.name} and I am ${this.age}`;
+  }
+}
+// now that we created the class (the blueprint) we have to instantiate a NEW object using this class)
+const person_1 = new Person("JOHN", 22);
+const person_2 = new Person("George", 33); //the console.log runs bc the constructor runs it when we instanciate the obj from the class
+
+// console.log(person_2.name, person_2.age);
+// console.log(person_2.greet());
+
 // ======== SUBCLASSES ========
+class Customer extends Person {
+  // if we want to inherit the properties of Person we have to use a func called SUPER
+  constructor(name, age, balance) {
+    // pass in the properties you want to use that belong to Person
+    // they are part of the parent constructor
+    super(name, age);
+    this.balance = balance;
+  }
+
+  info() {
+    return `${this.name} owes $${this.balance}`;
+  }
+}
+
+const customer1 = new Customer("Kevin", 32, 300);
+console.log(customer1.info());
+// console.log(customer1.name);
 
 // ======== MODULES ========
+
+// // file 1 (file1.js)
+// const name = 'Jeff';
+// // if we want that variable available in file2.js we can export it/anything (obj, classed etc..)
+// export const name = 'jeff';
+// export const numns = [1,2,3];
+// export default Person;
+
+// // file 2 (file2.js)
+// import { name, nums } from './file1';
+// import Person from './file1';
+
+// console.log(name);
+// =>> jeff

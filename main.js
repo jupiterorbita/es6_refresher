1a5e94a60a4f75ad7e1c584a28a6420f8f07efe5
jupterorbita <john.williams@outlook.com>
2018-12-10 20:44:17 -0700

    const,let, arrow function, foreach, map, filter

--- main.js
+++ main.js
@@ -1,25 +1,66 @@
-// CONST & LET
+// ======== CONST & LET ========
 let name = "John";
 let test; // you can init a var with let without giving it a value
 //const test2; // cannot do that with const
-
 name = "Jack";
+// console.log(name);
 
-console.log(name);
-// ARROW FUNCTIONS
+const person = {
+  name: "John",
+  age: 33
+};
+person.name = "Jack";
+// console.log(person);
 
-// FOREACH
+const nums = [1, 2, 3, 4];
+nums.push(5);
+// console.log(nums);
 
-// MAP
+// ======== ARROW FUNCTIONS ========
 
-// FILTER
+// function sayHello() {
+//   console.log("Hello");
+// }
+// sayHello();
 
-// SPREAD
+// const sayHello = () => {
+//   console.log("arrow Hello");
+// };
+// sayHello();
 
-// DESTRUCTURING
+const sayHello = name => console.log(`Hello ${name}!!!`);
 
-// CLASSES
+// sayHello("Johnny");
 
-// SUBCLASSES
+const fruits = ["apples", "oranges", "grapes"];
+// ======== FOREACH ========
+fruits.forEach(function() {}); // could do that but... better arrow function
 
-// MODULES
+fruits.forEach((fruit, index) => console.log(fruit));
+
+// ======== MAP ========
+//becauase it is going to return something, an array here, we will put this .map in a var
+const singleFruit = fruits.map(fruit => fruit.slice(0, -1).toUpperCase());
+// console.log(singleFruit);
+
+// ======== FILTER ========
+const people = [
+  { id: 1, name: "Karen" },
+  { id: 2, name: "Bob" },
+  { id: 3, name: "Sharon" }
+];
+console.log(people);
+// console.log(JSON.stringify(people));
+
+const people2 = people.filter(person => person.id != 2);
+console.log(people2);
+
+// ======== SPREAD ========
+
+// ======== DESTRUCTURING ========
+
+// ======== CLASSES ========
+
+// ======== SUBCLASSES ========
+
+// ======== MODULES ========

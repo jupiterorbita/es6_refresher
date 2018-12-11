254871efeea86dd83c303df948b1238436a0880a
jupterorbita <john.williams@outlook.com>
2018-12-10 19:42:57 -0700

    es6 refresh

---
+++ main.js
@@ -0,0 +1,25 @@
+// CONST & LET
+let name = "John";
+let test; // you can init a var with let without giving it a value
+//const test2; // cannot do that with const
+
+name = "Jack";
+
+console.log(name);
+// ARROW FUNCTIONS
+
+// FOREACH
+
+// MAP
+
+// FILTER
+
+// SPREAD
+
+// DESTRUCTURING
+
+// CLASSES
+
+// SUBCLASSES
+
+// MODULES
